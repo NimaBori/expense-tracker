acf431568b03519f8e1b01d40887f72109fa7db3
NimaBori <114718041+NimaBori@users.noreply.github.com>
2023-02-11 09:04:20 +0800

    create Dashboard component.

--- src/components/cards/Dashboard.js
+++ src/components/cards/Dashboard.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import PieChartWithNeedle from "./PieChartWithNeedle";
+import { useGlobalContext } from "../context";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { transactions } = useGlobalContext();
+  const currentBalance = transactions
+    .map((transact) => transact.amount)
+    .reduce((acc, cur) => acc + cur, 0);
+  let incomes = [];
+  let expenses = [];
+  transactions.map((transac) => {
+    if (transac.type === "income") {
+      incomes.push(transac.amount);
+    } else {
+      expenses.push(transac.amount);
+    }
+  });
+  const totalExpenses = expenses.reduce((acc, cur) => acc + cur, 0);
+  console.log("dashboard incomes: ", typeof Math.min(...expenses));
+  return (
+    <Container>
+      <h2>Dashboard</h2>
+      <PieChartWithNeedle
+        status={currentBalance}
+        title="BALANCE"
+        maximum={Math.max(...incomes)}
+      />
+      <PieChartWithNeedle
+        status={Math.abs(totalExpenses)}
+        title="SPENDING"
+        maximum={Math.abs(Math.min(...expenses))}
+      />
+    </Container>
+  );
 };
 
 export default Dashboard;
